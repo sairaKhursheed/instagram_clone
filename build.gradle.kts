b5f5b89955eb04021d76c7939ce0e71e3804c986
sairaKhursheed <94365392+sairaKhursheed@users.noreply.github.com>
2025-07-01 21:19:48 +0500

    push all code to git

---
+++ build.gradle.kts
@@ -0,0 +1,10 @@
+import com.android.build.gradle.internal.dsl.decorator.SupportedPropertyType.Collection.List.type
+
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+plugins {
+    alias(libs.plugins.androidApplication) apply false
+}
+
+
+
+
